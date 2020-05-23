d13ba26db611da79098a60339427c0c0fe314b06
Julia Homa <blumik100@gmail.com>
2020-05-23 19:39:24 +0300

    Green_orange

---
+++ Quiz_green/app/src/main/java/com/example/android/quiz/GameOverFragment.kt
@@ -0,0 +1,22 @@
+package com.example.android.quiz
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.android.quiz.databinding.FragmentGameOverBinding
+
+//Фрагмент проигрыша
+class GameOverFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_over, container,false)
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
+        }
+        return binding.root
+    }
+}
